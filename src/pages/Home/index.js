00e0b40495a8740dbe0ddd6b28ede0f3548ecf3d
Andreia Buca <andreia.buca@hotmail.com>
2024-07-15 16:03:37 +0200

    Display the latest event in the footer vignette.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,9 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const { data } = useData()
+  // récuperer le dernière évenement (prestation)
+  const last = data?.events.sort((evtA, evtB) => new Date(evtB.date) - new Date(evtA.date))[0];
   return <>
     <header>
       <Menu />
@@ -121,7 +123,8 @@ const Page = () => {
           title={last?.title}
           date={new Date(last?.date)}
           small
-          label="boom"
+          // affiche le nom correcte pour la prestation
+          label={last?.type}
         />
       </div>
       <div className="col contact">
